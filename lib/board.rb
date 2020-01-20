7e50e20a3c0d92c6ccf9b0604a28609ccfb808f5
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-20 17:04:57 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -36,7 +36,11 @@ class Board
   end 
   
   def valid_move?(input)
-    
+    input.to_i.between?(1, 9) && !taken?(input)
+  end 
+  
+  def update(input, player)
+    cells[input.to_i-1]
   end 
   
   
