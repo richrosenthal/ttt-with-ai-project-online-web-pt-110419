2f0c0fa56437ae5b147501b0e1eb8b339cca6476
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-20 15:49:46 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,13 @@
 class Board
   attr_accessor :cells
   
+  
   def reset!
-    
-    @cells = Array.new()
+    @cells = Array.new(9, " ")
+  end 
+  
+  def initialize
+    reset!
   end 
 end   
   
