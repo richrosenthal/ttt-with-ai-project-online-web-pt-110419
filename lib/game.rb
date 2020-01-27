fc4576db90874922ae5771a1919480396b39ed8a
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-01-27 03:49:03 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -29,7 +29,8 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
-      @board.cells[winner[1]] == @board.cells[winner[2]] 
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
+      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
     end 
   end 
   
@@ -37,17 +38,50 @@ class Game
     @board.full? && !won?
   end 
     
-  def winner
-    if winning_combo = won?
-      @winner = @board.cells[winning_combo.first]
-    end 
-  end 
+  # def winner
+  #   if winning_combo = won?
+  #     @winner = @board.cells[winning_combo.first]
+  #   end 
+  # end 
   
+def winner
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (@board.cells[win_combo[0]]) == "X" && (@board.cells[win_combo[1]]) == "X" && (@board.cells[win_combo[2]]) == "X"
+      return "X"
+    elsif (@board.cells[win_combo[0]]) == "O" && (@board.cells[win_combo[1]]) == "O" && (@board.cells[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
+  end
+end
 
-    
+  # def turn
+  #   puts "It's now #{current_player.token}'s turn."
+  #   input = current_player.move(board, timer).to_i
+  #   if board.valid_move?(input.to_s)
+  #     board.update(input, current_player)
+  #     system('clear')
+  #     puts "Game #{@counter}" if @wargame
+  #     board.display
+  #   elsif input.between?(1, 9) == false
+  #     puts "That is an invalid move"
+  #     turn
+  #   else
+  #     puts "Whoops! Looks like that position is taken"
+  #     turn
+  #   end
+  # end  
 
   
-
+# def play
+#     turn until over?
+#     if won?
+#       puts "Congratulations #{winner}!"
+#     elsif draw?
+#       puts "Cat's Game!"
+#     end
+#   end
     
 
 
